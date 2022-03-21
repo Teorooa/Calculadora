4a697ff620dbcb9185a26e110c0b232376c96f92
Teorooa <101207025+Teorooa@users.noreply.github.com>
2022-03-21 08:26:18 -0500

    Create Cal.sh

---
+++ Cal.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+menu {
+
+
+
+printf "119199 sumar
+peintf "191199 restar
+printf "191991 muliplicar
+read -p " Presione 1 para continuar
+
+elif [[ $option == 1 ]][[ $option == 01 ]]; then
+
+read -p "enter: " y x
+read -p "enter: " l m
+read -p "enter: " h n
+
+echo $((y + x)
